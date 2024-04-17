e064edc95d295a8f6f396abc95f816ef706c0fe7
rnujhgd <yhm02277@gmail.com>
2024-04-17 12:46:24 +0900

    20240417

--- app.js
+++ app.js
@@ -6,12 +6,20 @@ const { resourceUsage } = require('process');
 
 // Express 애플리케이션을 생성합니다.
 const app = express();
+const session = require('express-session');
+const bodyParser = require('body-parser');
 
 // 기본 포트를 설정하거나 3000 포트를 사용합니다.
 const PORT = process.env.PORT || 3000;
 
 app.use('/static', express.static(path.join(__dirname, 'public')));
 app.use(express.json());
+app.use(session({
+  secret: '임의의_비밀키',
+  resave: false,
+  saveUninitialized: false,
+}));
+app.use(bodyParser.json())
 
 const users = [
   {id: "hong", name: '홍길동', pwd: '1234'},
@@ -39,6 +47,23 @@ app.get('/user', (req, res) => {
   }  
 });
 
+app.get('/session/logout', (req, res) => {
+  req.session.destroy();
+  res.send('로그아웃 성공');
+});
+
+app.post('/session/login', (req, res) => { 
+  const { id, pwd } = req.body;
+  const user = users.find(user => user.id === id && user.pwd === pwd);
+
+  if (user) {
+    req.session.user = {id: user.id, name: user.name};
+    res.send('로그인 성공');
+  } else {
+    res.status(401).send('로그인 실패');
+  }
+});
+
 app.post('/user', (req, res) => {
   const { id, name, pwd } = req.body;
   
@@ -47,19 +72,33 @@ app.post('/user', (req, res) => {
     return userData.id === id
   });
 
-  if(pwd.length < 8) {
-    return res.send('pwd가 너무 짧습니다. (8자 이상)');
-  }
-  
   if(resultUser) {
-      return res.send('이미 있는 사용자입니다');
+    res.status(400).send('이미 있는 사용자입니다');
+  }else{
+    if(pwd.length >= 8) {
+      res.json({ message: `${id},${name},${pwd}를 받았습니다.` });
+    } else {
+      res.status(400).send('pwd가 너무 짧습니다. (8자 이상)');
     }
-
-  return res.json({ message: `${id},${name},${pwd}를 받았습니다.` });
+  }
   });
 
 app.put('/user', (req, res) => {
-  res.send('유저 수정')
+  const {id,name,pwd} =req.body;
+  const existingUser = users.find(user => user.id === id);
+  if(!existingUser) {
+    return res.status(400).send({message:'해당 사용자는 존재하지 않습니다'})
+  }
+  if(pwd && pwd.length < 8) {
+    return res.status(400).send({message : '비밀번호는 8자리 이상'})
+  }
+  if(name) {
+    existingUser.name = name;
+  }
+  if(pwd) {
+    existingUser.pwd = pwd;
+  }
+  res.json('업데이트');
 });
 
 app.delete('/user', (req, res) => {
